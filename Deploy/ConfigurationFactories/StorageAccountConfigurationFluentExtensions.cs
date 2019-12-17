155c1476a68a2d19af5d1e30b2d527065165a5f7
codyconfer <mail@codyconfer.me>
2019-12-16 23:22:22 -0700

    deployment project

---
+++ Deploy/ConfigurationFactories/StorageAccountConfigurationFluentExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+namespace HelloWorld.Deploy.ConfigurationFactories
+{
+    public static class StorageAccountConfigurationFluentExtensions
+    {
+        public static AccountArgs DefineResourceGroup(this AccountArgs storageAccountArgs, ResourceGroup resourceGroup)
+        {
+            storageAccountArgs.ResourceGroupName = resourceGroup.Name;
+            return storageAccountArgs;
+        }
+    }
+}
