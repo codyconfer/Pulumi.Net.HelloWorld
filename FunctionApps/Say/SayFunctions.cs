f11130b48b3dbc0bb96c9ecb0c2ce7b63eeca96b
codyconfer <mail@codyconfer.me>
2019-12-18 10:54:31 -0700

    replace function app

---
+++ FunctionApps/Say/SayFunctions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Say
+{
+    public static class SayFunctions
+    {
+        public const string ReturnMessageAppSettingKey = "ReturnMessage";
+
+        [FunctionName(nameof(Say))]
+        public static Task<IActionResult> Say(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req,
+            ILogger log) => Task.FromResult(
+                new OkObjectResult(Environment.GetEnvironmentVariable(ReturnMessageAppSettingKey)) as IActionResult
+            );
+                
+    }
+}
